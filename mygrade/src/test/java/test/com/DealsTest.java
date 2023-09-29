c4bc6a09b6327af7929c080e8ee5a5ee7b32ae42
devanshi2806 <59164658+devanshi2806@users.noreply.github.com>
2023-09-29 18:41:34 -0500

    this is my deals test

---
+++ mygrade/src/test/java/test/com/DealsTest.java
@@ -0,0 +1,73 @@
+package test.com;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DealsTest {
+
+    private WebDriver driver;
+    private String baseUrl;
+
+    public DealsTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "c:\\data\\chromedriver.exe");
+        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+      //  options.addArguments("--disable-gpu");
+    //    options.addArguments("--window-size=1400,800");
+        WebDriver driver = new ChromeDriver(options);
+         
+        //baseUrl = "https://www.google.com/";
+        //driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);  // implicit wait// it wait until 30 sec
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+        driver.close();
+    }
+
+    @Test
+    public void testDeal() throws Exception {
+
+        driver.manage().window().maximize();
+        // implicit wait condition
+        driver.get("https://www.costco.com/");
+
+        WebElement elem1 = driver.findElement(By.xpath("//*[@id=\"Home_Ancillary_2\"]"));
+        Actions act = new Actions(driver);
+        Action a1 = act.moveToElement(elem1).build();
+        a1.perform();
+        //explicit wait
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Home & Kitchen")));
+        driver.findElement(By.linkText("Home & Kitchen")).click();
+        driver.findElement(By.xpath("//div[@id='WC_ContentAreaESpot_div_1_ProductsSpotlight']/div[2]/ul/li/div[2]/a/div")).click();
+        driver.findElement(By.xpath("//img[@alt='Barcalounger Bradford Executive Office Chair']")).click();
+        driver.findElement(By.id("__richfx_id_0_2_1790176_850889_2")).click();
+    }
+
+}
